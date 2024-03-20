06873c672d845515713a1f0f7359e53c6dd836d9
AhmedMeligy <ahmed.tarek540540@gmail.com>
2024-03-20 05:27:04 +0200

    react-lab-5

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
 
-export default function Navbar() {
+
+
+const Navbar = ({ cartItems }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
@@ -16,9 +19,20 @@ export default function Navbar() {
               <li className="nav-item">
               <Link className="nav-link" to="/addproducts">AddProducts</Link>
               </li>
+              <li className="nav-item">
+              <Link className="nav-link" to="/addproducts"><div>Cart ({cartItems.length})</div></Link>
+              </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    cartItems: state.cart.items,
+  };
+};
+
+export default connect(mapStateToProps)(Navbar);
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,44 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setProducts } from '../../actions/productsActions';
 import axios from 'axios';
-import useFetch from '../../Fetch';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { addToCart } from '../../actions/cartActions';
 
-const Products = () => {
-    const { data: products, loading, fetchData } = useFetch("http://localhost:2000/products");
+const Products = ({ addToCart }) => {
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.products.products);
 
-    const handleDelete = (productId) => {
-        axios
-          .delete(`http://localhost:2000/products/${productId}`)
-          .then(() => fetchData())
-          .catch(err => console.log(err));
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('http://localhost:2000/products');
+        dispatch(setProducts(response.data));
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
     };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+    fetchProducts();
+  }, [dispatch]);
 
-    if (loading) return <div>Loading...</div>;
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  };
 
-    return (
-        <div className='row mt-4 '>
-            {products.map((product) => (
-                <div key={product.id} className="col-md-3 d-flex justify-content-center">
-                    <div className="card mb-4" style={{ width: '18rem' }}>
-                        <img src={product.thumbnail} className="card-img-top" alt={product.name} style={{ height: '200px', objectFit: 'cover' }} />
-                        <div className="card-body">
-                            <h5 className="card-title">{product.title}</h5>
-                            <p className="card-text">Price: {product.price}</p>
-                            <div className="d-flex justify-content-between">
-                                <button onClick={() => handleDelete(product.id)} className='btn bg-danger text-white'>Delete</button>
-                                <Link className='btn btn-primary' to={`/products/${product.id}`}>View</Link>
-                                <Link className='btn bg-success text-white' to={`/edit/${product.id}`}>Edit</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
+  if (!products || products.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className='row mt-4'>
+      {products.map((product) => (
+        <div key={product.id} className="col-md-3 d-flex justify-content-center">
+          <div className="card mb-4" style={{ width: '18rem' }}>
+            <img src={product.thumbnail} className="card-img-top" alt={product.name} style={{ height: '200px', objectFit: 'cover' }} />
+            <div className="card-body">
+              <h5 className="card-title">{product.title}</h5>
+              <p className="card-text">Price: {product.price}</p>
+              <div className="d-flex justify-content-between">
+                <button onClick={() => handleAddToCart(product)} className='btn btn-primary'>Add to cart</button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
-export default Products;
+const mapDispatchToProps = {
+  addToCart: addToCart,
+};
+
+export default connect(null, mapDispatchToProps)(Products);
